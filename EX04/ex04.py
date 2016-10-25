576a8ccc6673d2d15349bc1fc66c66463536342d
Yan Ka Man <Kevinpro@ws56133.eduroam.haw-hamburg.de>
2016-10-25 12:56:01 +0200

    added folders & excercises 1-14

---
+++ EX04/ex04.py
@@ -0,0 +1,13 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # iches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s."
+print
+print
+print
+print
