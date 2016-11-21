d76cbe18148e1fa684513fd85824fb680269f290
Yan Ka Man <Kevinpro@KEVINs-MacBook-Pro-3.local>
2016-11-21 22:34:11 +0100

    add until ex48

---
+++ EX48/template/ex48/lexicon.py
@@ -0,0 +1,13 @@
+
+# first = ('direction',' north')
+# second = ('verb', 'go')
+# third = ('stop', 'the')
+# fourth = ('noun', 'bear')
+# fifth =  ('number', '1234')
+# sixth = ('error', 'ASDFADFASDF')
+# sentence = {first, second, third, fourth, fifth, sixth}
+
+
+
+def scan(s): 
+	pass
